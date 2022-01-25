9c253c461709753114828ee148a430b420ea24a6
Maadi5 <55384421+Maadi5@users.noreply.github.com>
2022-01-25 17:40:50 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -126,7 +126,7 @@ def detect(save_img=False):
                     if save_img or view_img:  # Add bbox to image
                         label = f'{names[int(cls)]} {conf:.2f}'
                         print(xyxy, 'dims')
-                        h2blu
+#                         h2blu
                         bb_im = im0[xywh[1]: xywh[1] + xywh[3], xywh[0]:xywh[0] + xywh[2]]
                         color = get_hardhat_clr(bb_im)
                         #plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
